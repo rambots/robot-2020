4489669f2ad109b03059a95679d3325e7bd2a9be
Mahim <mahim216@gmail.com>
2020-01-05 23:58:12 -0500

    removing local rampage lib

--- src/main/java/com/rambots4571/infiniterecharge/robot/Robot.java
+++ src/main/java/com/rambots4571/infiniterecharge/robot/Robot.java
@@ -1,6 +1,27 @@
 package com.rambots4571.infiniterecharge.robot;
 
+import com.rambots4571.rampage.joystick.DriveStick;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
+    public final static DriveStick leftStick = new DriveStick(0);
+    public final static DriveStick rightStick = new DriveStick(1);
+
+    @Override
+    public void robotInit() {}
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void autonomousInit() {}
+
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void teleopInit() {}
+
+    @Override
+    public void teleopPeriodic() {}
 }
