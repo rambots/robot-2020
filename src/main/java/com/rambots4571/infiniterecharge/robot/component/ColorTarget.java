2ac1afaceaa2dadca50d3f4d04e53016631ce2c5
Mahim <mahim216@gmail.com>
2020-02-06 01:06:39 -0500

    added getComplement()

--- src/main/java/com/rambots4571/infiniterecharge/robot/component/ColorTarget.java
+++ src/main/java/com/rambots4571/infiniterecharge/robot/component/ColorTarget.java
@@ -1,5 +1,36 @@
 package com.rambots4571.infiniterecharge.robot.component;
 
 public enum ColorTarget {
-    Blue, Green, Red, Yellow, Unknown
+    Blue {
+        @Override
+        public ColorTarget getComplement() {
+            return Red;
+        }
+    },
+    Green {
+        @Override
+        public ColorTarget getComplement() {
+            return Yellow;
+        }
+    },
+    Red {
+        @Override
+        public ColorTarget getComplement() {
+            return Blue;
+        }
+    },
+    Yellow {
+        @Override
+        public ColorTarget getComplement() {
+            return Green;
+        }
+    },
+    Unknown {
+        @Override
+        public ColorTarget getComplement() {
+            return Unknown;
+        }
+    };
+
+    public abstract ColorTarget getComplement();
 }
