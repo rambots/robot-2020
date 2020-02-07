204d525aeca64047ff773e7736cc0df80535053a
Mahim <mahim216@gmail.com>
2020-02-07 17:14:41 -0500

    Update Drivetrain.java

--- src/main/java/com/rambots4571/infiniterecharge/robot/subsystem/Drivetrain.java
+++ src/main/java/com/rambots4571/infiniterecharge/robot/subsystem/Drivetrain.java
@@ -1,39 +1,43 @@
 package com.rambots4571.infiniterecharge.robot.subsystem;
 
 import com.rambots4571.infiniterecharge.robot.Constants;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-
-import java.awt.*;
 
 public class Drivetrain extends SubsystemBase {
     private static Drivetrain instance;
 
     private CANSparkMax leftMaster =
-            new CANSparkMax(Constants.Drive.leftMaster,
+            new CANSparkMax(
+                    Constants.Drive.leftMaster,
                     CANSparkMaxLowLevel.MotorType.kBrushless);
 
     private CANSparkMax rightMaster =
-            new CANSparkMax(Constants.Drive.rightMaster,
+            new CANSparkMax(
+                    Constants.Drive.rightMaster,
                     CANSparkMaxLowLevel.MotorType.kBrushless);
 
     private Drivetrain() {
         CANSparkMax leftFollowerOne =
-                new CANSparkMax(Constants.Drive.leftFollowerOne,
+                new CANSparkMax(
+                        Constants.Drive.leftFollowerOne,
                         CANSparkMaxLowLevel.MotorType.kBrushless);
         leftFollowerOne.follow(leftMaster);
         CANSparkMax leftFollowerTwo =
-                new CANSparkMax(Constants.Drive.leftFollowerTwo,
+                new CANSparkMax(
+                        Constants.Drive.leftFollowerTwo,
                         CANSparkMaxLowLevel.MotorType.kBrushless);
         leftFollowerTwo.follow(leftMaster);
 
         CANSparkMax rightFollowerOne =
-                new CANSparkMax(Constants.Drive.rightFollowerOne,
+                new CANSparkMax(
+                        Constants.Drive.rightFollowerOne,
                         CANSparkMaxLowLevel.MotorType.kBrushless);
         rightFollowerOne.follow(rightMaster);
         CANSparkMax rightFollowerTwo =
-                new CANSparkMax(Constants.Drive.rightFollowerTwo,
+                new CANSparkMax(
+                        Constants.Drive.rightFollowerTwo,
                         CANSparkMaxLowLevel.MotorType.kBrushless);
         rightFollowerTwo.follow(rightMaster);
 
