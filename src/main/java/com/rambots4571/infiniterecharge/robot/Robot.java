ac4b780cca5c5561b6928e82b1ecfa4ad7951767
Mahim <mahim216@gmail.com>
2020-02-13 21:04:00 -0500

    scheduler set

--- src/main/java/com/rambots4571/infiniterecharge/robot/Robot.java
+++ src/main/java/com/rambots4571/infiniterecharge/robot/Robot.java
@@ -1,6 +1,7 @@
 package com.rambots4571.infiniterecharge.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
     private RobotContainer container;
@@ -11,7 +12,12 @@ public class Robot extends TimedRobot {
     }
 
     @Override
-    public void disabledInit() {}
+    public void robotPeriodic() {}
+
+    @Override
+    public void disabledInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
 
     @Override
     public void disabledPeriodic() {}
@@ -20,11 +26,15 @@ public class Robot extends TimedRobot {
     public void autonomousInit() {}
 
     @Override
-    public void autonomousPeriodic() {}
+    public void autonomousPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
     @Override
     public void teleopInit() {}
 
     @Override
-    public void teleopPeriodic() {}
+    public void teleopPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 }
